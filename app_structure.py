6f2fa61057279284fd72e7176e707b74f1573342
PauloRamiro <64112617+PauloRamiro@users.noreply.github.com>
2023-07-12 04:28:06 -0300

    Update app_structure.py

--- app_structure.py
+++ app_structure.py
@@ -96,12 +96,10 @@ class Body:
         difference = df_ticker["Adj Close"].copy() - predict["yhat"].iloc[:-(number_periods_forecast)].copy()
         average_difference = difference.iloc[-15:].mean()
         difference = DataFrame(difference.iloc[-15:].to_list() + [average_difference]*number_periods_forecast, columns=["difference"])
-        st.write(difference)
 
         corrected_predict = DataFrame(predict["yhat"].iloc[-(number_periods_forecast+15):].to_list(), columns =["yhat"])
         corrected_predict = corrected_predict["yhat"].copy() + difference["difference"].copy()
-    
-        st.write(corrected_predict)
+        
         predict = predict.iloc[-(number_periods_forecast+15):]
 
         #st.write(predict[['ds', 'yhat', 'yhat_lower', 'yhat_upper']])
